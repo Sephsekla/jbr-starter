e97fe3e55726059a5ccccddcf88201721842f139
Joe Bailey-Roberts <dev@joebr.io>
2021-05-24 14:02:08 +0100

    Add quotes

--- create.sh
+++ create.sh
@@ -1,3 +1,3 @@
 echo "Please enter the name of your theme";
 read theme_name;
-grep -rl '|_NAME_|' ./ | LC_ALL=C xargs sed -i '' s/|_NAME_|/$theme_name/g;
+grep -rl '|_NAME_|' ./ | LC_ALL=C xargs sed -i '' "s/|_NAME_|/$theme_name/g";
