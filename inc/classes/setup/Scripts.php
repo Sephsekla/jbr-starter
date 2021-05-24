52c1efdec87b6329e3ea94b81ae7a285edd4f6da
Joe Bailey-Roberts <dev@joebr.io>
2021-05-24 13:17:13 +0100

    Add editor enqueing

--- inc/classes/setup/Scripts.php
+++ inc/classes/setup/Scripts.php
@@ -12,6 +12,8 @@ class Scripts {
 	public function __construct() {
 
 		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue' ) );
+		add_action( 'enqueue_block_assets', array( $this, 'editor_blocks' ) );
+
 
 	}
 
@@ -24,5 +26,15 @@ class Scripts {
 
 		wp_enqueue_script( 'fercor-scripts' );
 
+	}
+
+	public function editor_blocks(){
+		wp_register_style( 'fercor-editor-styles', get_template_directory_uri() . '/dist/editor.min.css', array(), filemtime( get_template_directory() . '/dist/editor.min.css' ), 'all' );
+
+		wp_enqueue_style( 'fercor-editor-styles' );
+
+		wp_register_script( 'fercor-block-scripts', get_template_directory_uri() . '/dist/blocks.js', array( 'wp-blocks', 'wp-i18n', 'wp-element', 'wp-editor', 'wp-data', 'wp-components' ), filemtime( get_template_directory() . '/dist/blocks.js' ), true );
+
+		wp_enqueue_script( 'fercor-block-scripts' );
 	}
 }
