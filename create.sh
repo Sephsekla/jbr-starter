c9bbdb8a7f5273f92af07443fb5aa75d58d725b4
Joe Bailey-Roberts <dev@joebr.io>
2021-05-24 14:01:14 +0100

    Prompt for user input

--- create.sh
+++ create.sh
@@ -1,1 +1,3 @@
-echo "Creating"; grep -rl '|_NAME_|' ./ | LC_ALL=C xargs sed -i '' 's/|_NAME_|/bar/g';
+echo "Please enter the name of your theme";
+read theme_name;
+grep -rl '|_NAME_|' ./ | LC_ALL=C xargs sed -i '' s/|_NAME_|/$theme_name/g;
