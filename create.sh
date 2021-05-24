46d91a041f28776a6ff24bfd30f1b78072b40709
Joe Bailey-Roberts <dev@joebr.io>
2021-05-24 14:28:43 +0100

    Separate out js and php package names

--- create.sh
+++ create.sh
@@ -1,14 +1,18 @@
-echo "Please enter the name of your theme";
+echo "Please enter the name of your theme e.g. My Awesome Theme";
 read theme_name;
-grep -rl '|_NAME_|' ./ | LC_ALL=C xargs sed -i '' "s/|_NAME_|/$theme_name/g";
+grep -rl --exclude-dir={node_modules,.git,vendor} '|_NAME_|' ./ | LC_ALL=C xargs sed -i '' "s/|_NAME_|/${theme_name//\//\\/}/g";
 
-echo "Please enter the name of your package (for NPM and composer)";
-read package_name;
-grep -rl '|_PACKAGE_|' ./ | LC_ALL=C xargs sed -i '' "s/|_PACKAGE_|/$package_name/g";
-
-echo "Please enter your theme slug";
+echo "Please enter your theme slug e.g. my_awesome_theme";
 read theme_slug;
-grep -rl '|_SLUG_|' ./ | LC_ALL=C xargs sed -i '' "s/|_SLUG_|/$theme_slug/g";
+grep -rl --exclude-dir={node_modules,.git,vendor} '|_SLUG_|' ./ | LC_ALL=C xargs sed -i '' "s/|_SLUG_|/${theme_slug//\//\\/}/g";
+
+echo "Please enter the name of your PHP package (for Composer) e.g. vendor/package";
+read package_name;
+grep -rl --exclude-dir={node_modules,.git,vendor} '|_PACKAGE_|' ./ | LC_ALL=C xargs sed -i '' "s/|_PACKAGE_|/${package_name//\//\\/}/g";
+
+echo "Please enter the name of your JS package (for NPM) e.g. @scope/package";
+read js_package_name;
+grep -rl --exclude-dir={node_modules,.git,vendor} '|_JS_PACKAGE_|' ./ | LC_ALL=C xargs sed -i '' "s/|_JS_PACKAGE_|/${js_package_name//\//\\/}/g";
 
 echo "Installing dependencies";
 pnpm install && composer install;
